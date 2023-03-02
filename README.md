ebcb4b825b5c490dad5c3aa7ca5bfeb736daa3d9
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-03-02 19:55:03 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# movie_app
+
+A Flutter project where I am learning and applying to properly organize files and folder Structure
+Also I am using 'The Movie Database' for the data.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home/view/view.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
