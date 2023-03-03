ea19444f5194442dbb9ca25312d5b8ddd97ae17e
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-03-03 23:10:40 +0545

    movie model and repository create

---
+++ lib/models/movie.dart
@@ -0,0 +1,14 @@
+
+
+
+class Movie{
+  final int id;
+  final String poster;
+
+  const Movie({
+    required this.id,
+    required this.poster,
+  });
+}
+
+
---
+++ lib/repositories/constants.dart
@@ -0,0 +1,65 @@
+import '../models/movie.dart';
+
+const List<Movie> movieList = [
+  Movie(
+      id: 1,
+      poster: 'https://m.media-amazon.com/images/M/MV5BOTBjMjA4NmYtN2RjMi00YWZlLTliYTktOTIwMmNkYjYxYmE1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg'
+  ),
+  Movie(
+      id: 2,
+      poster: 'https://cdn.marvel.com/content/1x/snh_online_6072x9000_posed_01.jpg',
+  ),
+  Movie(
+      id: 3,
+      poster: 'https://m.media-amazon.com/images/M/MV5BODRlNTY5ZjktZjE0Ni00YjZhLTk3NTItYzk0ZjYxN2QxMWEzXkEyXkFqcGdeQXVyMjMwNDgzNjc@._V1_.jpg'
+  ),
+  Movie(
+    id: 4,
+    poster: 'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/shangchi_and_the_legend_of_the_ten_rings_500x.jpg?v=1622220347',
+  ),
+  Movie(
+    id: 5,
+    poster: 'https://lumiere-a.akamaihd.net/v1/images/pp_disney_blackpanther_wakandaforever_1289_d3419b8f.jpeg?region=0%2C0%2C540%2C810',
+  ),
+];
+
+
+const List<Movie> movieTabs = [
+  Movie(
+    id: 1,
+    poster: 'https://m.media-amazon.com/images/M/MV5BYWY1ZDY4MmQtYjhiYS00N2QwLTk1NzgtOWI2YzUwZThjNDYwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg',
+  ),
+  Movie(
+      id: 2,
+      poster: 'https://lumiere-a.akamaihd.net/v1/images/p_antmanquantumania_update_1006_122aeffe.jpeg?region=0%2C0%2C540%2C810'
+  ),
+  Movie(
+    id: 3,
+    poster: 'https://i0.wp.com/bloody-disgusting.com/wp-content/uploads/2022/11/last-of-us-tv-1.png?ssl=1',
+  ),
+  Movie(
+      id: 4,
+      poster: 'https://m.media-amazon.com/images/M/MV5BZTdhM2Q3YTQtNzg4Yi00MWI5LWIyODEtZDZkNjllZDYxYjg1XkEyXkFqcGdeQXVyODEwMTc2ODQ@._V1_FMjpg_UX1000_.jpg'
+  ),
+  Movie(
+    id: 5,
+    poster: 'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/shangchi_and_the_legend_of_the_ten_rings_500x.jpg?v=1622220347',
+  ),
+  Movie(
+    id: 6,
+    poster: 'https://lumiere-a.akamaihd.net/v1/images/pp_disney_blackpanther_wakandaforever_1289_d3419b8f.jpeg?region=0%2C0%2C540%2C810',
+  ),
+  Movie(
+      id: 7,
+      poster: 'https://m.media-amazon.com/images/M/MV5BODRlNTY5ZjktZjE0Ni00YjZhLTk3NTItYzk0ZjYxN2QxMWEzXkEyXkFqcGdeQXVyMjMwNDgzNjc@._V1_.jpg'
+  ),
+  Movie(
+    id: 8,
+    poster: 'https://m.media-amazon.com/images/M/MV5BZDA0MzcxZTgtMTAzZC00MGJkLTg3YzItZjMzNjkwOTVlODNlXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg',
+  ),
+  Movie(
+    id: 9,
+    poster: 'https://images.fandango.com/ImageRenderer/820/0/redesign/static/img/default_poster.png/0/images/masterrepository/fandango/229145/JesusRev_USATODAY_2000x3000_1A.jpg',
+  ),
+
+];
