c6269e835d2e17428aedb890321cd828be11d9df
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-03-08 21:50:43 +0545

    Api endpoints/api exception handling class create

---
+++ lib/api.dart
@@ -0,0 +1,9 @@
+
+
+class Api{
+  static const baseUrl = 'https://api.themoviedb.org/3';
+  static const getNowPlaying = '$baseUrl/movie/now_playing';
+  static const getUpcoming = '$baseUrl/movie/upcoming';
+  static const getTopRated = '$baseUrl/movie/top_rated';
+  static const getPopular = '$baseUrl/movie/popular';
+}
