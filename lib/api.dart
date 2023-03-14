a29d77adff3e29c3e4f0ae34b012161fd0c9ede5
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-03-14 21:48:23 +0545

    API key add

--- lib/api.dart
+++ lib/api.dart
@@ -1,9 +1,13 @@
 
 
 class Api{
+  static const apiKey = '9e1e9cb2692a10b10950a10368f5361c';
+
   static const baseUrl = 'https://api.themoviedb.org/3';
   static const getNowPlaying = '$baseUrl/movie/now_playing';
   static const getUpcoming = '$baseUrl/movie/upcoming';
   static const getTopRated = '$baseUrl/movie/top_rated';
   static const getPopular = '$baseUrl/movie/popular';
 }
+
+
