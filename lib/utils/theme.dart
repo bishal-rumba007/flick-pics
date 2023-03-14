5396baae1ed431e1068f5b4b2ecc5aee00577c96
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-03-14 21:53:47 +0545

    lib/utils/theme.dart

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/utils/color_manager.dart';
 
-import 'color_manager.dart';
 
 
 
 final darkTheme = ThemeData(
   brightness: Brightness.dark,
   primaryColor: Colors.green,
-
 );
+
+class Theme{
+  final customDark = ThemeData(
+    colorScheme: ColorScheme(
+        brightness: Brightness.dark,
+        primary: ColorManager.primary,
+        onPrimary: ColorManager.primary,
+        secondary: ColorManager.textGray,
+        onSecondary: ColorManager.inputColor,
+        error: Colors.red,
+        onError: Colors.red,
+        background: ColorManager.backGround,
+        onBackground: ColorManager.backGround,
+        surface: ColorManager.iconGray,
+        onSurface: Colors.black12,
+    )
+  );
+}
