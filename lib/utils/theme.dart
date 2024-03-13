36917d558d5d5a8ccb4ce9fa082da4b2f61a4017
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-03-13 22:39:01 +0545

    update

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -7,22 +7,17 @@ import 'package:movie_app/utils/color_manager.dart';
 final darkTheme = ThemeData(
   brightness: Brightness.dark,
   primaryColor: Colors.green,
+  colorScheme: ColorScheme(
+      brightness: Brightness.dark,
+      primary: ColorManager.primary,
+      onPrimary: ColorManager.primary,
+      secondary: ColorManager.textGray,
+      onSecondary: ColorManager.inputColor,
+      error: Colors.red,
+      onError: Colors.red,
+      background: ColorManager.backGround,
+      onBackground: ColorManager.backGround,
+      surface: ColorManager.iconGray,
+      onSurface: Colors.black12,
+    ),
 );
-
-class Theme{
-  final customDark = ThemeData(
-    colorScheme: ColorScheme(
-        brightness: Brightness.dark,
-        primary: ColorManager.primary,
-        onPrimary: ColorManager.primary,
-        secondary: ColorManager.textGray,
-        onSecondary: ColorManager.inputColor,
-        error: Colors.red,
-        onError: Colors.red,
-        background: ColorManager.backGround,
-        onBackground: ColorManager.backGround,
-        surface: ColorManager.iconGray,
-        onSurface: Colors.black12,
-    )
-  );
-}
